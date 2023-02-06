4809bb2799b90f5b9b68d915b86c637777bcaf64
Bodhisattwa Basu <basubodhisattwa6@gmail.com>
2023-02-06 23:00:05 +0530

    first commit

---
+++ 3-module.js
@@ -0,0 +1,9 @@
+
+const names=require('./4-names')
+const sayHi=require('./5-utils')
+// console.log(names)
+const data= require('./6-alt')
+require('./7-mg')
+console.log(data)
+sayHi('susan')
+sayHi(names.john)
---
+++ 4-names.js
@@ -0,0 +1,7 @@
+//local
+const secret='SUPER SECRET'
+//share
+const john='john'
+const bodhi='bodhi'
+
+module.exports = {john,bodhi}
